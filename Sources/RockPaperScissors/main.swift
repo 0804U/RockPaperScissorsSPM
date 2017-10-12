f115ee92d53993c4f31c357e9394ac34c633a880
rikner <rikner@web.de>
2017-10-12 21:44:37 +0200

    input versus random

--- Sources/RockPaperScissors/main.swift
+++ Sources/RockPaperScissors/main.swift
@@ -1,10 +1,24 @@
 import Glibc
+import Commander
 
 for count in 1...4 {
     print(count)
     sleep(1)
 }
 
-let randomItem = GameItem.getRandom();
-print(randomItem)
+let main = command { (item:String) in
+    guard let number = Int(item) else {
+        print("no number")
+        return
+    }
 
+    guard let item = GameItem(rawValue: number) else {
+        print("number must be 0 ..< " + String(describing: GameItem.count))
+        return
+    }
+
+    let randomItem = GameItem.getRandom();
+    print(String(describing: item) + " vs. " + String(describing: randomItem))
+}
+
+main.run()
